6d82beb0d1af7447b87987860b055d4a9313d91f
sr2072 <sr2072@rit.edu>
2024-10-21 16:54:02 -0400

    more structure!

---
+++ assets/inc/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageName; ?></title>
+</head>
+
+<body>
+    
+    <nav class="nav">
+        <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Contact</li>
+            <li>Resume</li>
+        </ul>
+    </nav>
+</body>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $pageName = "Saurav's Portfolio";
+    include("assets/inc/header.php");
+?>
+
+<head>
+    <link href="assets/css/index.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <h1>Hi, my name is Saurav Raut</h1>
+    <p class="subText">Liverpool, NY | 315-416-2019 | sr2072@rit.edu </p>
+    <h2>About me</h2>
+    <p class="about">I am a Software Engineering Student.</p>
+</body>
+</html>
